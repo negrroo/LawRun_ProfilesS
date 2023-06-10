f1fed05251bf2b2c4100085b004bd28b0cfe56b9
negrroo <mohammedaelnaggar1@gmail.com>
2023-06-10 09:45:39 +0200

    Profiles: v4.2.1_Test00.1
    Try to add magisk 26.1 support
    
    Signed-off-by: negrroo <mohammedaelnaggar1@gmail.com>

--- service.sh
+++ service.sh
@@ -1,12 +1,15 @@
 #!/system/bin/sh
+# Conditional MagiskHide properties
 
-MODDIR=${0%/*}
-
-while [ "$(getprop sys.boot_completed)" != 1 ]; do
- sleep 2
-done
+# Late props which must be set after boot_completed
+{
+    until [[ "$(getprop sys.boot_completed)" == "1" ]]; do
+        sleep 1
+    done
 
 # Permissions
 chown root /system/etc/LawRun/LRK.init.sh
 chmod +x /system/etc/LawRun/LRK.init.sh
 /system/etc/LawRun/LRK.init.sh &
+
+}&
