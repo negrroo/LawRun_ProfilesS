bb18352f2949197e0188e52f6e25d4297819222a
negrroo <mohammedaelnaggar1@gmail.com>
2024-04-22 21:50:57 +0200

    Upstreamed: profiles to v4.4...
    -Upstream profiles module to v4.4
    -New permissions to support KernelSU
    -Other improvements..
    
    Signed-off-by: negrroo <mohammedaelnaggar1@gmail.com>

--- service.sh
+++ service.sh
@@ -2,10 +2,11 @@
 
 MODDIR=${0%/*}
 
+# Permissions
+chmod +x /data/adb/modules/LawRunProfiles/includes/LRK.init.sh
+
 while [ "$(getprop sys.boot_completed)" != 1 ]; do
  sleep 2
 done
 
-# Permissions
-chmod 777 data/adb/modules/LawRunProfiles/includes/*
-./data/adb/modules/LawRunProfiles/includes/LRK.init.sh
+cd / && ./data/adb/modules/LawRunProfiles/includes/LRK.init.sh
